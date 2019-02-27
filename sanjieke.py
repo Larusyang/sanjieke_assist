f1b0a50f99ae30925c73ee1e1f88508ceb4e9e64
larusyang <ygq1101@gmail.com>
2019-02-27 19:00:34 +0800

    数据入库sqlite3

--- sanjieke.py
+++ sanjieke.py
@@ -1,5 +1,6 @@
 import requests
-
+import sqlite3
+import json
 
 class Login(object):
     def __init__(self):
@@ -16,6 +17,8 @@ class Login(object):
         self.homework_detail_url ='https://assist.sanjieke.cn/correct/homeworkDetail?homework_id={}' # 作业评价接口
         self.praise_record_url ='https://assist.sanjieke.cn/correct/praiseRecords?page={}' #作业点赞接口
         self.session = requests.Session()
+        self.conn = sqlite3.connect('sanjieke.db')
+
 #模拟登录
     def login(self,phone,password):
         post_data = {
@@ -24,7 +27,9 @@ class Login(object):
         }
         response = self.session.post(self.login_url,data=post_data,headers=self.headers)
         if response.status_code == 200:
-            print(response.text)
+            print('登录成功！')
+        else:
+            print('登录失败，请重试!')
 #获取批改总量
     def get_correct_total(self):
         response = self.session.get(self.summary_url)
@@ -42,31 +47,81 @@ class Login(object):
             }
             response = self.session.post(self.correct_list_url,data=post_data,headers=self.headers)
             if  page < int(response.json()['data']['total_count'])/10 + 1: #校验抓取循环是否超过最大列数
-                data = response.json()['data']['correct_list']
-                print(data)
-                print('-----已完成一项')
+                correct_list = response.json()['data']['correct_list']
+                for data in correct_list:
+                    print(data)
+                    self.insert_correct_list(data)
+                    self.get_comment_info(data['homework_id'])
+
+                print('-----已完成一项-----')
                 page+= 1
             else:
                 print('抓取完毕')
                 break
 #获取助教对学员的作业点评内容
     def get_comment_info(self,homework_id):
-        response = self.session.get(self.homework_detail_url.format(homework_id))
-        comment = response.json()['data']['correct_list'][0]['content']
-        print(comment)
+        try:
+            response = self.session.get(self.homework_detail_url.format(homework_id))
+            comment = response.json()['data']['correct_list'][0]
+            comment.update({'homework_id':homework_id})
+            self.insert_comment_info(comment)
+        except:
+            pass
+
 
 #获取作业点赞信息
     def get_praise_record(self,page =1):
-        response = self.session.get(self.praise_record_url.format(page))
-        data = response.json()['data']['records']
-        print(data)
+        while True:
+            response = self.session.get(self.praise_record_url.format(page))
+            data = response.json()['data']['records']
+            print(data)
+#保存批改明细到数据库
+    def insert_correct_list(self,data):
+        self.conn.execute('''create table if not exists correct_list
+        (homework_id Int primary key not null,
+        correct_time text not null,
+        class_id int not null,
+        question_title text not null,
+        create_time text not null,
+        status int,
+        grade int,
+        recommend int,
+        comment_score int,
+        name text,
+        teacher_name text);''')
+        sql = '''insert or ignore into correct_list(homework_id,correct_time,class_id,question_title,create_time,status,grade,recommend,comment_score,name,teacher_name) VALUES
+('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')''' %(data['homework_id'],data['correct_time'],data['class_id'],data['question_title'],data['create_time'],data['status'],data['grade'],data['recommend'],data['comment_score'],data['name'],data['teacher_name'])
+        self.conn.execute(sql)
+        self.conn.commit()
+        #关闭数据库连接
+        #self.conn.close()
+#保存作业点评到数据库
+    def insert_comment_info(self,data):
+        self.conn.execute('''create table if not exists comment_info
+        (homework_id int primary key not null,
+        name text,
+        content text,
+        score int,
+        correct_time text,
+        recommend int
+        );''')
+        sql = '''insert or ignore into comment_info(homework_id,name,content,score,correct_time,recommend) VALUES
+        ('%s','%s','%s','%s','%s','%s')''' %(data['homework_id'],data['name'],data['content'],data['score'],data['correct_time'],data['recommend'])
+        self.conn.execute(sql)
+        self.conn.commit()
+
+    def inquery_sanjieke(self):
+        #批改记录总数
+        sql_correct_total = '''select count(distinct(homework_id)) from correct_list where status != 0'''
+        total = self.conn.execute(sql_correct_total)
+        print(total)
 
 
+    
 
 if __name__ == '__main__':
     login =Login()
     login.login('15295730742','ygq123456')
     login.get_correct_total()
-   # login.get_comment_info('11904207')
-   # login.get_correct_list()
-    login.get_praise_record()
+    #login.get_correct_list()
+    login.inquery_sanjieke()
